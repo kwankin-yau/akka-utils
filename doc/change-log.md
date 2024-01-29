d77c18ff906841b95429c56b4cba8c2ac1687ca4
KwanKin Yau <alphax@vip.163.com>
2024-01-29 22:31:11 +0800

    # 1.1.0 [2024-01-29] ## Improvement - `Buncher` and `BunchReqBatch` add id property.

---
+++ doc/change-log.md
@@ -0,0 +1,6 @@
+# 1.1.0 [2024-01-29]
+## Improvement
+- `Buncher` and `BunchReqBatch` add id property. 
+
+# 1.0 [2024-01-04]
+initial commit
--- src/main/scala/com/lucendar/akkautils/patterns/Buncher.scala
+++ src/main/scala/com/lucendar/akkautils/patterns/Buncher.scala
@@ -13,23 +13,24 @@ object Buncher {
   /**
    * Create Buncher Props.
    *
+   * @param buncherId The ID of the buncher.
    * @param target The target actor finally receives and handles the `BuncherReqBatch` message.
    * @param interval The interval of collecting `BunchReq` messages and merge them into a single `BuncherReqBatch` message.
    * @param maxSize the maximum batch size. The Buncher will build a `BuncherReqBatch` message and send it to the `target`
    *                when the count of cached messages reaches `maxSize`.
    * @return The `Props` for the Buncher.
    */
-  def props(target: ActorRef, interval: FiniteDuration, maxSize: Int): Props =
-    Props(new Buncher(target, interval, maxSize))
+  def props(buncherId: String, target: ActorRef, interval: FiniteDuration, maxSize: Int): Props =
+    Props(new Buncher(buncherId, target, interval, maxSize))
 
 
   case class ReqAndSender(req: Object, sndr: ActorRef)
   case class BunchReq(req: Object)
-  case class BunchReqBatch(requests: Vector[ReqAndSender])
+  case class BunchReqBatch(buncherId: String, requests: Vector[ReqAndSender])
 }
 
 
-class Buncher(target: ActorRef, interval: FiniteDuration, maxSize: Int) extends Actor with Timers {
+class Buncher(id: String, target: ActorRef, interval: FiniteDuration, maxSize: Int) extends Actor with Timers {
 
   import Buncher._
 
@@ -44,7 +45,7 @@ class Buncher(target: ActorRef, interval: FiniteDuration, maxSize: Int) extends 
   private def active(buffer: Vector[ReqAndSender]): Receive = {
     case Timeout =>
       if (buffer.nonEmpty)
-        target ! BunchReqBatch(buffer)
+        target ! BunchReqBatch(id, buffer)
 
       context.become(idle)
 
@@ -52,7 +53,7 @@ class Buncher(target: ActorRef, interval: FiniteDuration, maxSize: Int) extends 
       val newBuffer = buffer :+ ReqAndSender(m.req, sender())
       if (newBuffer.size == maxSize) {
         timers.cancel(TimerKey)
-        target ! BunchReqBatch(newBuffer)
+        target ! BunchReqBatch(id, newBuffer)
         context.become(idle)
       } else
         context.become(active(newBuffer))
